f02d600f91ad254838e0158022d6b0cb84889c93
Asrul Kadir <asrulkadir.asus4@gmail.com>
2023-08-09 21:25:44 +0800

    connecting to mongodb

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,26 @@
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  'mongodb+srv://nasa-api:k2VefK4onZQjPX95@nasacluster.yachntu.mongodb.net/?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connected!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
