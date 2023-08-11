597dbde41567f00be190935b0382259e4949fdef
Asrul Kadir <asrulkadir.asus4@gmail.com>
2023-08-11 22:04:58 +0800

    create schema

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+  // Add a new property to the schema called "lastUpdated" with a type of Date.
+  lastUpdated: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
--- server/src/server.js
+++ server/src/server.js
@@ -6,7 +6,8 @@ const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = process.env.MONGO_URL;
+const MONGO_URL =
+  'mongodb+srv://nasa-api:k2VefK4onZQjPX95@nasacluster.yachntu.mongodb.net/?retryWrites=true&w=majority';
 
 const server = http.createServer(app);
 
