debb9740b7f46f08e2bac65ef1f9dfcef3a1c4d4
Asrul Kadir <asrulkadir.asus4@gmail.com>
2023-09-11 22:12:45 +0800

    persisting spaceX launches

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -42,6 +42,11 @@ async function populateLaunches() {
     },
   });
 
+  if (response.status !== 200) {
+    console.log('Problem downloading launch data');
+    throw new Error('Launch data download failed');
+  }
+
   const launchDocs = response.data.docs;
   for (const launchDoc of launchDocs) {
     const payloads = launchDoc['payloads'];
@@ -60,7 +65,7 @@ async function populateLaunches() {
     };
     console.log(`${launch.flightNumber} ${launch.mission}`);
 
-    // TODO: populate launches collection...
+    await saveLaunch(launch);
   }
 }
 
@@ -100,14 +105,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-  const planet = await planets.findOne({
-    keplerName: launch.target,
-  });
-
-  if (!planet) {
-    throw new Error('No matching planet was found');
-  }
-
   await launchesDatabase.findOneAndUpdate(
     {
       flightNumber: launch.flightNumber,
@@ -120,6 +117,13 @@ async function saveLaunch(launch) {
 }
 
 async function scheduleNewLaunch(launch) {
+  const planet = await planets.findOne({
+    keplerName: launch.target,
+  });
+
+  if (!planet) {
+    throw new Error('No matching planet was found');
+  }
   const newFlightNumber = (await getLatestFlightNumber()) + 1;
   const newLaunch = Object.assign(launch, {
     success: true,
